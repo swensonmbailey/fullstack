8dd531f64f7e730d68b4b20bae3120d842a11ec5
Swenson <swensonmbailey@gmail.com>
2024-09-27 20:49:49 -0500

    working

---
+++ cms/src/app/contacts/contacts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-contacts',
+  standalone: true,
+  imports: [],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.css'
+})
+export class ContactsComponent {
+
+}
---
+++ cms/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
